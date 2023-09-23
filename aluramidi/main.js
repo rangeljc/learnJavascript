30f2af61f8202f8f079a6165e70e9f7b6bdb4dc8
rangeljc <rangeljc@gmail.com>
2023-09-23 20:58:55 -0300

    Solucionando erros das teclas

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -2,7 +2,7 @@ function tocar_som(id_botao) {
     
     const elemento = document.querySelector(id_botao);
     
-    if (elemento.localname === 'audio') {
+    if (elemento.localName === 'audio') {
         elemento.play();
     }
     else {
@@ -12,20 +12,6 @@ function tocar_som(id_botao) {
 }
 
 const lista_teclas = document.querySelectorAll('.tecla');
-
-/*
-let i = 0;
-
-while (i < lista_teclas.length){
-    
-    const tecla = lista_teclas[i];
-    const botao = `#som_${tecla.classList[1]}` //template string
-    tecla.onclick = function () {
-        tocar_som(botao);
-    }
-    i += 1;
-}
-*/
 
 for (i=0; i < lista_teclas.length; i++) {
 
